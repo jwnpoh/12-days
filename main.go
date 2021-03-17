5cd48f81c490f32ea307a371d9561b3a76803f5e
jwnpoh <jwn.poh@gmail.com>
2021-03-17 14:26:36 +0800

    import math/big to take care of huge numbers

--- main.go
+++ main.go
@@ -3,58 +3,43 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 	"strconv"
-	//"os"
 )
 
 const usage = "How many days of Christmas is your true love gifting you gifts for?"
 
 func main() {
-
-	// get number of days from command line arguments (os and strconv required)
-	/*	arg := os.Args
-
-			// If no arguments given.
-			if len(arg) != 2 {
-				fmt.Println(usage)
-				return
-			}
-
-			// get number of days
-			d, err := strconv.Atoi(arg[1])
-
-
-		if err != nil {
-			fmt.Println(usage + " (in numbers)")
-			return
-		}
-	*/
-
 	// get number of days with user input with fmt.Scan
-	var u string
+	var input string
 
 	fmt.Println(usage)
-	fmt.Scan(&u)
+	fmt.Scan(&input)
 
 	// make sure that only integers input
-	d, err := strconv.ParseUint(u, 10, 64)
+	d, err := strconv.ParseUint(input, 10, 64)
 
 	if d < 0 || err != nil {
 		fmt.Println("This makes no sense.")
 		return
 	}
 
-	var sum, total uint64
+	e := big.NewInt(int64(d))
 
-	for i := uint64(1); i <= d; i++ {
-		sum += i
-		total += sum
+	sum, total := big.NewInt(0), big.NewInt(0)
+
+	for i := big.NewInt(1); i.Cmp(e) != 1; {
+		sum.Add(sum, i)
+		total.Add(total, sum)
+
+		i.Add(i, big.NewInt(int64(1)))
 	}
-	fmt.Printf("The total number of gifts you would receive on the last day is %d.\n", sum)
+
+	fmt.Printf("The total number of gifts you would receive on the last day is %v.\n", sum)
 
 	if d == 1 {
-		fmt.Printf("The sum total number of gifts received over %d day is %d.\n", d, total)
+		fmt.Printf("The sum total number of gifts received over %d day is %v.\n", d, total)
 	} else {
-		fmt.Printf("The sum total number of gifts received over %d days is %d.\n", d, total)
+		fmt.Printf("The sum total number of gifts received over %d days is %v.\n", d, total)
 	}
 }
